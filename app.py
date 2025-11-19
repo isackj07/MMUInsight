6700373438f8ea3aae1261015a2eba4dfdeab3f6
Jimmy <jimhanhor@gmail.com>
2025-11-20 01:05:21 +0800

    Connected css for login's html

--- app.py
+++ app.py
@@ -1,17 +1,14 @@
-import os
 from flask import Flask, render_template
+import os
 from extensions import db, bcrypt
-from models import User  
-from auth import auth_bp
+from models import User, Subject, Review
+from auth import auth_bp  # FIX
 
 app = Flask(__name__)
 
-app.config["SECRET_KEY"] = "isac_is_a_monkey67"
-
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-db_path = os.path.join(BASE_DIR, "database", "mmuinsight.db")
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + db_path
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db_path = os.path.join(BASE_DIR, 'database', 'mmuinsight.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + db_path
 
 db.init_app(app)
 bcrypt.init_app(app)
@@ -20,8 +17,9 @@ app.register_blueprint(auth_bp)
 
 @app.route("/")
 def index():
-    return render_template("register.html")
+    return render_template('register.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
+
 
